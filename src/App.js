18cf8ef0bf4be7bc980ad8c5a84da481e696b831
Likit <likitchamuthai@gmail.com>
2023-04-28 11:30:23 +0700

    update file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Navbar,Header,Footer} from './func-components'
-import Calendar from './class-components'
-import MsgBox from './func-props'
+//import {Navbar,Header,Footer} from './func-components'
+//import Calendar from './class-components'
+//import MsgBox from './func-props'
+//import MsgBox2 from './class-props'
+import List from './list-comp'
 
 function App() {
- 
- return ([<Navbar />,<p><Calendar /></p>,<Header />,<Footer />]
+  return (<List />)
+ //return ([<Navbar />,<p><Calendar /></p>,<Header />,<Footer />]
 
   /*
   <>
@@ -15,12 +17,12 @@ function App() {
     <Footer />
   </>*/
 
- ) 
+ //) 
 
  /*return(
-     <MsgBox 
+     <MsgBox2 
         text="Useing props in function component"
-        color="red"
+        color="green"
         bgColor="#ccc"
         fontSize="16pt"
         border="dotted 1px black"
